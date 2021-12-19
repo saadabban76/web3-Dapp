c8df7f539c790e80cc7fafa8731951edc0fc6229
thesaaddevloper <thesaaddeveloper@gmail.com>
2021-12-19 16:10:34 +0530

    challenge updated

---
+++ components/Avatar.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import React from 'react'
+import { useMoralis } from 'react-moralis'
+
+function Avatar({ username, logoutOnPress}) {
+    const {user, logout} = useMoralis();
+    return (
+        <Image
+         className='relative rounded-full cursor-pointer bg-black
+         hover:opacity-75'
+         src={`https://avatars.dicebear.com/api/pixel-art/${username || user.get('username')}.svg`}
+         layout='fill'
+         onClick={() => logoutOnPress && logout()}
+      />
+    );
+}
+
+export default Avatar
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import Login from '../components/login';
 import { useMoralis } from 'react-moralis';
 import Image from 'next/image';
+import Header from '../components/Header';
+import Messages from '../components/Messages';
 
 export default function Home() {
 
-  const { isAuthenticated, isInitializing, logout} = useMoralis();
+  const { isAuthenticated, isInitializing} = useMoralis();
 
   if(isInitializing) return (
 <div class=" bg-black flex items-center justify-center h-screen w-full ">
@@ -32,14 +34,17 @@ export default function Home() {
   if(!isAuthenticated) return <Login />;
 
   return (
-    <div className="">
+    <div className="h-screen overflow-y-scroll 
+    bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the App</h1>
-      <button onClick={logout}>Logout</button>
-
+        <div className='max-w-screen-2xl mx-auto'>
+          {/* header */}
+          <Header />
+          <Messages />
+        </div>
     </div>
   )
 }
